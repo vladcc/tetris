0a68106d45a1576e8bb49f99e4595caa65266f09
vladcc <vld.dinev@gmail.com>
2016-09-04 22:58:48 +0300

    Update tetris.h

--- tetris/tetris.h
+++ tetris/tetris.h
@@ -17,12 +17,12 @@
 
 /* ---------------- CONSTANTS  ----------------*/
 #define MATRIX_SIZE			5				// make sure there is enough space for rotation
-#define PIVOT_POINT 		22				// marks the pivot point of rotation
-#define SHAPE_ELEMENTS		4				// how many elements does the shape have?
+#define PIVOT_POINT 			22				// marks the pivot point of rotation
+#define SHAPE_ELEMENTS			4				// how many elements does the shape have?
 #define FIELD_ROWS 			21				// first row always empty
 #define FIELD_COLS 			10				// 10 columns is classic tetris width
-#define FRAME_BUFF_ROWS 	FIELD_ROWS - 1
-#define FRAME_BUFF_COLS 	FIELD_COLS
+#define FRAME_BUFF_ROWS 		FIELD_ROWS - 1
+#define FRAME_BUFF_COLS 		FIELD_COLS
 #define LEFT				0				// LEFT and RIGHT are used to distinguish rotation
 #define RIGHT				1
 
@@ -30,9 +30,9 @@
 #define SQUARE				'0'
 #define CANDY_CANE			'0'
 #define Z_SHAPE				'0'
-#define TRAFFIC_SIGN		'0'
+#define TRAFFIC_SIGN			'0'
 #define STICK				'0'
-#define NUMBER_OF_SHAPES	7
+#define NUMBER_OF_SHAPES		7
 #define FIELD				' '
 #define BORDER				'#'
 
@@ -41,19 +41,19 @@
 #define MOVE_RIGHT			'd'
 #define FAST_DOWN			's'
 #define ROTATE_LEFT			'w'
-#define ROTATE_RIGHT		'e'
+#define ROTATE_RIGHT			'e'
 #define QUIT_GAME			'q'
 #define RESET_GAME			'r'
 #define PAUSE_GAME			'p'
 
 // gameplay
-#define SLEEP_MS			16							// check input 1000 / SLEEP_MS times a second
-#define FPS_RATE			1000 / SLEEP_MS				// fps rate
-#define LEVEL_1_SPEED		FPS_RATE / 2				// half a second starting speed
-#define LEVEL_UP			10							// how many points level you up
-#define MAX_LEVEL			15							// max level
-#define DOWN_QUICKER		10							// move DOWN_QUICKER times faster
-#define SPEED_RESOLUTION	LEVEL_1_SPEED / MAX_LEVEL 	// calculates equal speed increments
+#define SLEEP_MS			16				// check input 1000 / SLEEP_MS times a second
+#define FPS_RATE			1000 / SLEEP_MS			// fps rate
+#define LEVEL_1_SPEED			FPS_RATE / 2			// half a second starting speed
+#define LEVEL_UP			10				// how many points level you up
+#define MAX_LEVEL			15				// max level
+#define DOWN_QUICKER			10				// move DOWN_QUICKER times faster
+#define SPEED_RESOLUTION		LEVEL_1_SPEED / MAX_LEVEL 	// calculates equal speed increments
 /* LEVEL_1_SPEED / MAX_LEVEL = equalt speed decrements to max speed
  * of FPS_RATE times a second */
 
@@ -65,19 +65,19 @@ const int down[2] 		= 	{1, 0};		// and recalculating the shape position
 /* ---------------- STRUCTS  ----------------*/
 // the shape on the field
 struct shape {
-	int (* grid)[MATRIX_SIZE];					// grid pointer
+	int (* grid)[MATRIX_SIZE];			// grid pointer
 	int coordinates[SHAPE_ELEMENTS][2]; 		// places of shape elements in it's grid
-	int (*field_coords)[2];						// the current coordinates on the field
-	int length;									// the longest part of the shape
-	const int * pivot;							// pivot point of rotation
-	int code;									// code for the shape
+	int (*field_coords)[2];				// the current coordinates on the field
+	int length;					// the longest part of the shape
+	const int * pivot;				// pivot point of rotation
+	int code;					// code for the shape
 };
 
 // the data for the shape on the field
 struct shape_data {
-	int grid[MATRIX_SIZE][MATRIX_SIZE];			// holds the grid for the shape
-	int pivot[2];								// holds the pivot point of rotation
-	int code;									// holds the code for the shape elements
+	int grid[MATRIX_SIZE][MATRIX_SIZE];		// holds the grid for the shape
+	int pivot[2];					// holds the pivot point of rotation
+	int code;					// holds the code for the shape elements
 };
 
 /* Note: the shape code replaces the non-zero elements of the grid on run time */
@@ -89,7 +89,7 @@ typedef struct shape_data shape_data;
 /* ---------------- FUNCTIONS  ----------------*/
 // core functions
 void calculate_field_coordinates(shape * tetr, int row, int col);
-void scan_field(void);						// scans for full rows, removes them, and updates the score
+void scan_field(void);				// scans for full rows, removes them, and updates the score
 void initialize_field(void);
 void reset_field(void);
 
@@ -129,11 +129,11 @@ bool ask_player(char * msg);
 void print_m(int row, int col, int m[row][col]); 	// for debugging
 
 // globals
-int field[FIELD_ROWS][FIELD_COLS];					// the game field
-int field_buffer[FIELD_ROWS][FIELD_COLS];			// used for comparison with the previous field state on render
+int field[FIELD_ROWS][FIELD_COLS];			// the game field
+int field_buffer[FIELD_ROWS][FIELD_COLS];		// used for comparison with the previous field state on render
 int buffer_matrix[MATRIX_SIZE][MATRIX_SIZE];		// the shape grid goes in here
 int buffer_matrix_copy[MATRIX_SIZE][MATRIX_SIZE];	// used as a copy for pre-calculating and checking position
-int m_temp[MATRIX_SIZE][MATRIX_SIZE];				// used to hold temporary matrix transformations
+int m_temp[MATRIX_SIZE][MATRIX_SIZE];			// used to hold temporary matrix transformations
 int shape_field_coordinates[SHAPE_ELEMENTS][2];		// the coodinats of every element of the shape on the field
 int buffer_shape_field_coords[SHAPE_ELEMENTS][2];	// used for pre-calculating and checking validity
 
@@ -158,57 +158,57 @@ shape_data * next_shape;
 
 // structures defining the shapes
 shape_data square	=  {	.grid 	= 	{	{0, 0, 0, 0}, 
-											{0, 1, 1, 0},
-											{0, 1, 1, 0},
-											{0, 0, 0, 0}	},
+							{0, 1, 1, 0},
+							{0, 1, 1, 0},
+							{0, 0, 0, 0}	},
 									
-							.pivot 	= 	{0, 0},			// 0, 0 - don't rotate
-							.code 	= 	SQUARE				};
+							.pivot 	= 	{0, 0},	 // 0, 0 - don't rotate
+							.code 	= 	SQUARE	};
 						  
 shape_data candy_cane_left	=	{	.grid	=	{	{0, 0, 0 ,0},
-													{0, 0, 0, 1},
-													{0, 1, 1 ,1},
-													{0, 0, 0, 0}	},
+									{0, 0, 0, 1},
+									{0, 1, 1 ,1},
+									{0, 0, 0, 0}	},
 													  
 									.pivot	=	{2, 2},
-									.code	=	CANDY_CANE			};
+									.code	=	CANDY_CANE	};
 
 shape_data candy_cane_right	=	{	.grid	=	{	{0, 0, 0, 0},
-													{0, 0, 0, 1},
-													{0, 1, 1, 1},
-													{0, 0, 0, 0}	},
+									{0, 0, 0, 1},
+									{0, 1, 1, 1},
+									{0, 0, 0, 0}	},
 									
 									.pivot	=	{2, 2},
-									.code	=	CANDY_CANE			};
+									.code	=	CANDY_CANE	};
 						  
 shape_data traffic_sign	=	{	.grid	=	{	{0, 0, 0, 0},
-												{0, 0, 1, 0},
-												{0, 1, 1, 1},
-												{0, 0, 0, 0}	},
+								{0, 0, 1, 0},
+								{0, 1, 1, 1},
+								{0, 0, 0, 0}	},
 												
 								.pivot	=	{2, 2},
-								.code	=	TRAFFIC_SIGN		};
+								.code	=	TRAFFIC_SIGN	};
 							 
 shape_data z_shape_left	=	{	.grid	=	{	{0, 0, 0, 0},
-												{0, 1, 1, 0},
-												{0, 0, 1, 1},
-												{0, 0, 0, 0}	},
+								{0, 1, 1, 0},
+								{0, 0, 1, 1},
+								{0, 0, 0, 0}	},
 												
 								.pivot	=	{2, 2},
-								.code	=	Z_SHAPE				};
+								.code	=	Z_SHAPE		};
 								  
 shape_data z_shape_right	=	{	.grid	=	{	{0, 0, 0, 0},
-													{0, 0, 1, 1},
-													{0, 1, 1, 0},
-													{0, 0, 0, 0}	},
+									{0, 0, 1, 1},
+									{0, 1, 1, 0},
+									{0, 0, 0, 0}	},
 									
 									.pivot	=	{2, 2},
-									.code	=	Z_SHAPE				};
+									.code	=	Z_SHAPE	};
 
 shape_data stick	=	{	.grid	=	{	{0, 0, 0, 0, 0},
-											{0, 0, 0, 0, 0},
-											{0, 1, 1, 1, 1},
-											{0, 0, 0, 0, 0}	},
+								{0, 0, 0, 0, 0},
+								{0, 1, 1, 1, 1},
+								{0, 0, 0, 0, 0}	},
 											
 							.pivot	=	{2, 2},
-							.code	=	STICK				};
+							.code	=	STICK	};
